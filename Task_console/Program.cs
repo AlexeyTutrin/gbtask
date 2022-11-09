1b5bfa8c5b61f151414a1e5751e07ea5998b21be
Alexey Tyutrin <leha.tyutrin@mail.ru>
2022-11-10 00:05:18 +0400

    new info readme

---
+++ Task_console/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+
+
+string[] StringsArray = new string[] { "hello", "2", "world", "br" };
+string[] NewArray = new string[StringsArray.Length];
+
+void NewArrayWithWords(string[] FirstArray, string[] SecondArray)
+{
+    int result = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[result] = FirstArray[i];
+            result++;
+        }
+    }
+}
+
+void PrintArray(string[] Array)
+{
+    Write("[");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Write($"{Array[i]}, ");
+    }
+    Write("]");
+}
+
+NewArrayWithWords(StringsArray, NewArray);
+PrintArray(NewArray);
